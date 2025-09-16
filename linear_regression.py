bfe8206c71fa3631ceb59092baeed17b2a529a41
Kaamya Shinde <kaamyashinde@gmail.com>
2025-09-16 18:41:11 +0200

    add hyperparameters

--- linear_regression.py
+++ linear_regression.py
@@ -2,10 +2,11 @@ import numpy as np
 
 class LinearRegression():
     
-    def __init__(self):
-        # NOTE: Feel free to add any hyperparameters 
-        # (with defaults) as you see fit
-        pass
+    def __init__(self, learning_rate = 0.1, epochs = 1000):
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.weights, self.bias = None, None
+        self.losses, self.train_accuracies = [], []
         
     def fit(self, X, y):
         """
